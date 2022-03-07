8996b972b88fd2773ed09df936dffbb864056392
mescudi wes <59785454+tellesiivan@users.noreply.github.com>
2022-03-06 22:38:30 -0800

    nav

---
+++ components/layout/TopNav.js
@@ -0,0 +1,11 @@
+import Navigation from "./Navigation";
+
+export default function TopNav() {
+  return (
+    <>
+      <header className="fixed z-50 flex items-center h-10 px-2 mx-auto rounded-full shadow w-w-full top-4 bg-main-nav left-8 right-8 navBlur row">
+        <Navigation />
+      </header>
+    </>
+  );
+}
