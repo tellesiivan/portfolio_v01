1eafe5b4506234147c3cb72a24b7de015b784c25
mescudi wes <59785454+tellesiivan@users.noreply.github.com>
2022-03-11 14:10:45 -0800

    nav

--- components/layout/TopNav.js
+++ components/layout/TopNav.js
@@ -3,7 +3,7 @@ import Navigation from "./Navigation";
 export default function TopNav() {
   return (
     <>
-      <header className="fixed z-50 flex items-center h-10 max-w-5xl px-2 mx-auto rounded-full shadow w-w-full top-4 bg-main-nav left-8 right-8 navBlur row ">
+      <header className="fixed z-50 flex flex-row items-center h-10 max-w-5xl px-2 mx-auto rounded-full shadow w-w-full top-4 bg-main-nav left-4 right-4 md:left-8 md:right-8 navBlur items-evenly">
         <Navigation />
       </header>
     </>
--- components/reUsable/Tooltip.js
+++ components/reUsable/Tooltip.js
@@ -3,7 +3,7 @@ import { FaReact } from "react-icons/fa";
 
 export default function Tooltip({ name, icon }) {
   return (
-    <div className="flex flex-row items-center justify-between px-4 py-2 mt-2 mr-2 rounded-full md:w-40 link bg-main-darkLight">
+    <div className="flex flex-row items-center justify-between px-4 py-2 mt-2 mr-2 rounded-full md:w-40 link bg-common-blk">
       {icon}
       <span className="ml-4 text-sm md:ml-0 text-text-main">{name}</span>
     </div>
